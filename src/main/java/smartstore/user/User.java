0f86bfd04ec20fe03c5eef101bea6741f5900ae0
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-05-22 09:20:06 +0900

    fix: delete generatevalue in User.id

--- src/main/java/smartstore/user/User.java
+++ src/main/java/smartstore/user/User.java
@@ -1,8 +1,6 @@
 package smartstore.user;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +15,6 @@ import lombok.Setter;
 public class User {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   private String userId;
   private String email;
